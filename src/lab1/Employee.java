c78e26f8bab9a5af5a23b2dc9f046acc05f95381
Mitch <Mitch@10.30.21.43>
2017-09-03 09:41:13 -0500

    Encapsulation

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -12,6 +12,9 @@ import java.util.Date;
  */
 public class Employee 
 {
+    
+    private final String Required = " is a required field.";
+
     private String firstName;
     private String lastName;
     private String ssn;
@@ -21,4 +24,85 @@ public class Employee
     private boolean movedIn;
     private String cubeId;
     private Date orientationDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if(firstName == null || firstName.isEmpty()) 
+        {
+            throw new IllegalArgumentException("First name" + Required);
+        }
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if(lastName == null || lastName.isEmpty()) 
+        {
+            throw new IllegalArgumentException("Last name" + Required);
+        }
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+    
 }
